66f98b999ec2f8b66e4e96d22fb6b96de57d8449
Joseph Massie <josephmmassie@gmail.com>
2024-12-10 21:46:28 -0800

    Added additional top margin to the login, register,  and authenticate pages

--- site/src/app/reset/layout.tsx
+++ site/src/app/reset/layout.tsx
@@ -6,7 +6,7 @@ export default function ResetLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <main className="w-full min-h-full flex flex-col gap-8 items-center">
+        <main className="mt-40 w-full min-h-full flex flex-col gap-8 items-center">
             <div className="min-h-full flex flex-col sm:flex-row gap-8">
                 <Image
                     className="m-auto mt-0"
